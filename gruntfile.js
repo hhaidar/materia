676e88a369b17cbefdc06a932cc4cf826e065de6
Houssam Haidar <houssam@sdelements.com>
2015-03-08 21:43:03 -0500

    Add grunt/gitignore

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            main: {
+                files: {
+                    'assets/css/style.css': 'assets/less/style.less'
+                }
+            }
+        },
+        watch: {
+            options: {
+                spawn: false, 
+            },
+            main: {
+                files: ['assets/less/**/*'],
+                tasks: ['less']
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            main: {
+                tasks: ['watch']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['concurrent:main']);
+}
